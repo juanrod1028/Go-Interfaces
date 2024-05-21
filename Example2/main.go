b3ce3448f2059eaab4650d64838234b3a6760cbd
juan-rodriguez_adlover <juan.rodriguez@avaldigitallabs.com>
2024-05-21 16:30:41 -0500

    examples more clear

--- Example2/main.go
+++ Example2/main.go
@@ -7,6 +7,7 @@ import (
 
 type Player interface {
 	kickBall()
+	name()
 }
 
 type FootbalPlayerTipe1 struct {
@@ -17,6 +18,9 @@ type FootbalPlayerTipe1 struct {
 func (f FootbalPlayerTipe1) kickBall() {
 	shot := f.stamina + f.power
 	fmt.Println("tipe 1 Shot", shot)
+}
+func (f FootbalPlayerTipe1) name() {
+	fmt.Print("jugador 1")
 }
 
 type FootbalPlayerTipe2 struct {
@@ -29,22 +33,43 @@ func (f FootbalPlayerTipe2) kickBall() {
 	shot := f.stamina + f.power + f.speed
 	fmt.Println("Tipe 2 Shot", shot)
 }
+func (f FootbalPlayerTipe2) name() {
+	fmt.Print("jugador 2")
+}
 
 func main() {
-	team := make([]Player, 11)
+	player1 := FootbalPlayerTipe1{
+		stamina: rand.Intn(10),
+		power:   rand.Intn(10),
+	}
+	player1.kickBall()
 
-	for i := 0; i < len(team)-1; i++ {
+	player2 := FootbalPlayerTipe2{
+		stamina: rand.Intn(10),
+		power:   rand.Intn(10),
+		speed:   rand.Intn(10),
+	}
+	player2.kickBall()
+
+	team := make([]Player, 4)
+
+	for i := 0; i < len(team)-2; i++ {
+		//i can change here the type of player whithout affect the logic
 		team[i] = FootbalPlayerTipe1{
 			stamina: rand.Intn(10),
 			power:   rand.Intn(10),
 		}
 	}
 
-	team[len(team)-1] = FootbalPlayerTipe2{
-		stamina: rand.Intn(10),
-		power:   rand.Intn(10),
-		speed:   rand.Intn(10),
+	for i := 2; i < len(team); i++ {
+		//i can change here the type of player whithout affect the logic
+		team[i] = FootbalPlayerTipe2{
+			stamina: rand.Intn(10),
+			power:   rand.Intn(10),
+			speed:   rand.Intn(10),
+		}
 	}
+
 	for i := 0; i < len(team); i++ {
 		team[i].kickBall()
 	}
